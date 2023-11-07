39016ff0fba9dfc1da56fcee40cb10d37a092f39
Kelvin Chukwu <kelvin.chukwu@mail.utoronto.ca>
2023-11-06 22:08:27 -0500

    remove box

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import * as React from 'react';
 import Container from '@mui/material/Container'
-import Box from '@mui/material/Box'
 import CssBaseline from '@mui/material/CssBaseline';
 import Paper  from '@mui/material/Paper';
 
