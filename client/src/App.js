23f54bf1723ff101bcd2efb99ea7222c6180d4d7
Kelvin Chukwu <chukwu.kelvin@yahoo.com>
2023-11-06 22:06:35 -0500

    paper started

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,17 @@ import * as React from 'react';
 import Container from '@mui/material/Container'
 import Box from '@mui/material/Box'
 import CssBaseline from '@mui/material/CssBaseline';
+import Paper  from '@mui/material/Paper';
 
 function App() {
   return (
     <React.Fragment>
       <CssBaseline/>
-      <Container maxWidth="sm">
-        <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }} />
+      <Container maxWidth="md">
+        {/* <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }} /> */}
+        <Paper sx={{height: '100vh'}}>
+          foobar
+        </Paper>
       </Container>
     </React.Fragment>
   );
